700d6d9c924a383875a1bcf4aa585a7bc2a6c1fc
Sean <110854028+ExpressOwl@users.noreply.github.com>
2022-12-21 21:41:24 +0000

    Cleaned up the code on Toast

--- toast.js
+++ toast.js
@@ -15,8 +15,8 @@ function generateToast({
   toastContainer.insertAdjacentHTML(
     "beforeend",
     `<p class='toast' 
-  style='background-color: ${background}'
-  color: ${color}
+  style='background-color: ${background};
+  color: ${color};
   animation-duration: ${length}'>
   ${message}
   </p>`
@@ -42,5 +42,22 @@ success.addEventListener("click", () => {
     length: "5000ms",
     // Works but will the toast will stay invisible and populate
     // the page if not removed after clicked, hence line 25
+  });
+});
+
+warning.addEventListener("click", () => {
+  generateToast({
+    message: "Are you positive?",
+    background: "hsl(350 100% 66.5%)",
+    color: "hsl(51 97.8% 12.1%)",
+  });
+});
+
+info.addEventListener("click", () => {
+  generateToast({
+    message: "Write that down, write that down!",
+    background: "hsl(51 97.8% 65.1%%)",
+    color: "hsl(51 97.8% 12.1%)",
+    length: "6000ms",
   });
 });
